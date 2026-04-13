60d75a7b1c5bb9142f78c250ae151025e952f8c5
ildanin <ilya.a.danilenko@gmail.com>
2026-04-13 19:11:48 +0300

    fix: add round function

--- chessAI/__init__.py
+++ chessAI/__init__.py
@@ -1,11 +1,9 @@
 from network import Network
-import numpy as np
-from config import INFO, ACTIVATOR, NORMALIZER, FACTOR_RANGE, BIAS_RANGE, ALPHA, CYCLES
 from positionClass import Position
 from notation.square import BoardMove, move_decode
-from data import position_encode
+from chessAI.data import position_encode
 
-class ChessAI(Network):
+class ChessAI(Network):    
     def process(self, position: Position) -> BoardMove:
         move = super().process(position_encode(position))
-        return move_decode(''.join(str(bit) for bit in move))
+        return move_decode(''.join(str(round(bit)) for bit in move))
