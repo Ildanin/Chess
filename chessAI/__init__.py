7ccbf6d35a76345ac967c8131a64720863fca6b8
ildanin <ilya.a.danilenko@gmail.com>
2026-04-13 23:28:51 +0300

    style: remove spaces

--- chessAI/__init__.py
+++ chessAI/__init__.py
@@ -1,9 +1,9 @@
-from network import Network, load
+from network import Network
 from positionClass import Position
 from notation.square import BoardMove, move_decode
 from chessAI.data import position_encode
 
-class ChessAI(Network):    
+class ChessAI(Network):
     def get_move(self, position: Position) -> BoardMove:
         move = super().process(position_encode(position))
         return move_decode(''.join(str(round(bit)) for bit in move))
