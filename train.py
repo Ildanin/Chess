cfdf89aed3c0f848a78a0251bb04100702894da0
ildanin <ilya.a.danilenko@gmail.com>
2026-04-13 22:27:27 +0300

    feat: add simple train algorithm

--- train.py
+++ train.py
@@ -1,8 +1,13 @@
 from chessAI import ChessAI
 from chessAI.data import get_data
 from positionClass import Position
+import numpy as np
 from config import INFO, ACTIVATOR, NORMALIZER, FACTOR_RANGE, BIAS_RANGE, ALPHA, CYCLES
 
 ai = ChessAI(INFO, ACTIVATOR, NORMALIZER, FACTOR_RANGE, BIAS_RANGE)
 
-train_x, train_y = get_data("data.txt")
+train_x, train_y = get_data("data.txt", 0, 1000)
+
+ai.train_stochastic_momentum(train_x, train_y, ALPHA, 0.2, CYCLES, 1000, True)
+
+ai.save("ChessAI.txt")
