b56a44a5fd79c0d92b9d92b920c34098a30f41a2
ildanin <ilya.a.danilenko@gmail.com>
2026-04-13 23:28:39 +0300

    feat: add test

--- train.py
+++ train.py
@@ -1,13 +1,19 @@
 from chessAI import ChessAI
 from chessAI.data import get_data
-from positionClass import Position
-import numpy as np
-from config import INFO, ACTIVATOR, NORMALIZER, FACTOR_RANGE, BIAS_RANGE, ALPHA, CYCLES
+from config import INFO, ACTIVATOR, NORMALIZER, FACTOR_RANGE, BIAS_RANGE, ALPHA, MOMENTUM_RATE, CYCLES
 
 ai = ChessAI(INFO, ACTIVATOR, NORMALIZER, FACTOR_RANGE, BIAS_RANGE)
 
 train_x, train_y = get_data("data.txt", 0, 1000)
 
-ai.train_stochastic_momentum(train_x, train_y, ALPHA, 0.2, CYCLES, 1000, True)
+ai.train_vanilla(train_x, train_y, ALPHA, CYCLES, True)
 
-ai.save("ChessAI.txt")
+test_x, test_y = get_data("data.txt", 1000, 1010)
+
+cost = 0
+for x, y in zip(test_x, test_y):
+    move = ai.process(x)
+    cost += ai.cost(y)
+print(cost/len(test_x))
+
+ai.save("ChessAI3.txt")
