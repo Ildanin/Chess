e3c844618d6293e4eb528b914fa60047f175eba5
ildanin <ilya.a.danilenko@gmail.com>
2026-04-13 19:34:00 +0300

    rename: process -> get_move

--- chessAI/__init__.py
+++ chessAI/__init__.py
@@ -4,6 +4,6 @@ from notation.square import BoardMove, move_decode
 from chessAI.data import position_encode
 
 class ChessAI(Network):    
-    def process(self, position: Position) -> BoardMove:
+    def get_move(self, position: Position) -> BoardMove:
         move = super().process(position_encode(position))
         return move_decode(''.join(str(round(bit)) for bit in move))
